a06e0d56f1e5a93389f4f4e5414a45ad5c6a0255
yechuan51 <96866332+yechuan51@users.noreply.github.com>
2023-02-27 21:54:49 +0800

    Added web server for playwright.

--- my_website/playwright.config.ts
+++ my_website/playwright.config.ts
@@ -83,8 +83,10 @@ export default defineConfig({
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: 'npm run start',
+    port: 3000,
+    timeout: 120 * 1000,  // 2 minutes
+    reuseExistingServer: !process.env.CI,
+  },
 });
